32f5c5f3521e3e2c644fdf1c361f27535d76b8db
Dypose-java <d.bolonkin0022@gmail.com>
2025-01-08 23:19:08 +0300

    add new tests,add in build.gradle run console test

---
+++ src/test/java/ConsoleTest/NegativeTest.java
@@ -0,0 +1,28 @@
+package ConsoleTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+@Tag("console")
+public class NegativeTest {
+    @Test
+    void test1(){
+        Assertions.assertTrue(false);
+    }
+    @Test
+    void test2() {
+        Assertions.assertTrue(false);
+    }
+    @Test
+    void test3(){
+        Assertions.assertTrue(false);
+    }
+    @Test
+    void test4(){
+        Assertions.assertTrue(false);
+    }
+    @Test
+    void test5(){
+        Assertions.assertTrue(false);
+    }
+}
---
+++ src/test/java/ConsoleTest/PozitiveTest.java
@@ -0,0 +1,29 @@
+package ConsoleTest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("console")
+public class PozitiveTest {
+    @Test
+    void test1(){
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void test2() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void test3(){
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void test4(){
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void test5(){
+        Assertions.assertTrue(true);
+    }
+}
