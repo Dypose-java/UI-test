135c49c8fe3ea465d3a1008931b5e5bd7c89722a
Dypose-java <d.bolonkin0022@gmail.com>
2025-03-13 13:44:05 +0300

    add new

--- src/test/java/randCheckApi/Main.java
+++ src/test/java/randCheckApi/Main.java
@@ -1,5 +1,6 @@
 package randCheckApi;
 
+import io.qameta.allure.AllureId;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
@@ -7,27 +8,32 @@ import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import specification.MainSpecification;
 
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static randCheckApi.Specification.mainRequestSpec;
 import static randCheckApi.Specification.mainResponseSpec;
-import static specification.MainSpecification.mainRequestSpecification;
-import static org.hamcrest.Matchers.*;
+
 @Tag("docs.spacexdata.com")
 public class Main {
-    static final String capsule_serial ="C112";
-@Severity(SeverityLevel.NORMAL)
-@Epic("docs.spacexdata.com")
-    @DisplayName("Проверка запроса get "+capsule_serial)
+    static final String capsule_serial = "C112";
+
+    @Severity(SeverityLevel.NORMAL)
+    @Epic("docs.spacexdata.com")
+    @DisplayName("Проверка запроса get " + capsule_serial)
     @Tag("Api")
     @Test
-    void checkPostmanApiTest(){
-
+    void checkPostmanApiTest() {
         given(mainRequestSpec())
                 .when()
-                .get("/capsules/"+capsule_serial)
+                .get("/capsules/" + capsule_serial)
                 .then().spec(mainResponseSpec(200)).body("capsule_serial", Matchers.is(capsule_serial));
 
+    }
+
+    @Test
+    @AllureId("36828")
+    void checkStep() {
+        step("Измения ручного на автоматизированный");
     }
 }
