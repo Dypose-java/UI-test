e521ae2ef7331014162e058e2a3ed60284de4c54
Dypose-java <d.bolonkin0022@gmail.com>
2025-03-18 19:11:11 +0300

    add new test

--- src/test/java/api_and_ui/present_example_sushi_wok/BlyatEbanniPost.java
+++ src/test/java/api_and_ui/present_example_sushi_wok/BlyatEbanniPost.java
@@ -33,9 +33,14 @@ public class BlyatEbanniPost {
     }
     @Test
     void fdsfsdfs(){
-given().contentType(ContentType.JSON).body("{\n" +
-        "    \"name\": \"morpheus\",\n" +
-        "    \"job\": \"leader\"\n" +
-        "}").post("https://reqres.in/api/users").then().log().all();
+        given().header("X-Requested-With","XMLHttpRequest")
+                .body("{\n" +
+                "    \"phone\":\"+7(968)845-06-92\",\n" +
+                "    \"code\":\"231130\"\n" +
+                "}")
+                .contentType(ContentType.JSON)
+                .header("Cookie","_ym_uid=1727291633479176596; _ym_d=1727291633; default-city=false; cityTextId=msk; lang=ru; connect.sid=s%3AhWBay4JCw8MSE56ftWyd9RTTkAky5evA.vR0qmDZ3J2O%2FDP7sbgZLaLGfOLV8qFgPn88eZGXN7Pk; _sticky_param=b; _gcl_au=1.1.144102499.1741887509; _ga_TE53H5X77H=GS1.1.1741887508.1.0.1741887508.60.0.0; _ga=GA1.1.948291890.1741887509; _ym_isad=1; _ym_visorc=w")
+                .post("https://sushiwok.ru/user/register_or_auth").then().log().all();
+
     }
 }
--- src/test/java/api_and_ui/present_example_sushi_wok/Main.java
+++ src/test/java/api_and_ui/present_example_sushi_wok/Main.java
@@ -25,22 +25,11 @@ public class Main {
         SushiConfig conf =SushiConfig.CONFIG;
         log=conf.getLog();
         pas=conf.getPas();
-        //RestAssured.baseURI=conf.getUrlApi();
-        RestAssured.baseURI="https://sushiwok.ru/";
+        RestAssured.baseURI=conf.getUrlApi();
         Configuration.baseUrl=conf.getUrlUI();
-
     }
-    String cookie="_ym_uid=1727291633479176596; _ym_d=1727291633; default-city=false; cityTextId=msk; lang=ru; connect.sid=s%3AhWBay4JCw8MSE56ftWyd9RTTkAky5evA.vR0qmDZ3J2O%2FDP7sbgZLaLGfOLV8qFgPn88eZGXN7Pk; _sticky_param=b; _gcl_au=1.1.144102499.1741887509; _ym_isad=1; _ym_visorc=w; _gid=GA1.2.1403743463.1741887588; _ga=GA1.1.948291890.1741887509; _ga_565PTV2E7Y=GS1.1.1741887587.1.1.1741887765.60.0.0; _ga_TE53H5X77H=GS1.1.1741887508.1.1.1741887765.5.0.0";
-//@Disabled("периписать неахуй")
     @Test
     void check(){
-        //given().when().header("X-Requested-With","XMLHttpRequest").get("user/isExist").then().statusCode(200).body("isUserExist",Matchers.is(false));
         UserSW build = UserSW.builder().code("812592").phone("+7(968)845-06-92").build();
-
-        given().body(build).contentType(ContentType.JSON).header("X-Requested-With","XMLHttpRequest")
-                .post("user/register_or_auth")
-                .then()
-                .log().all()
-                .body("user.id",Matchers.is(2126854));
     }
 }
